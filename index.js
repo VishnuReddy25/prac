632dbb89b49b60b9f6c63278cccb2a63e0d559e8
VishnuReddy25 <118453537+VishnuReddy25@users.noreply.github.com>
2023-11-21 15:58:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,92 +1,86 @@
+// Event listener for DOMContentLoaded event
 document.addEventListener('DOMContentLoaded', () => {
+  // Get the reference to the user table body
   const userTableBody = document.getElementById('userTableBody');
 
-  // Loop through localStorage items
-  for (const item in localStorage) {
-    // Check if the item key starts with 'user_'
-    if (item.startsWith('user_')) {
-      // Parse the JSON data stored in the item value
-      const userData = JSON.parse(localStorage.getItem(item));
+  // Loop through all localStorage items
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
 
-      // Create a new table row
-      const newRow = userTableBody.insertRow();
+    // Check if the key starts with 'user_'
+    if (key.startsWith('user_')) {
+      // Parse the JSON-stored user data from localStorage
+      const userData = JSON.parse(localStorage.getItem(key));
 
-      // Add cells for user data
-      const nameCell = newRow.insertCell();
-      nameCell.textContent = userData.name;
-      const emailCell = newRow.insertCell();
-      emailCell.textContent = userData.email;
-      const passwordCell = newRow.insertCell();
-      passwordCell.textContent = userData.password;
-      const dobCell = newRow.insertCell();
-      dobCell.textContent = userData.dob;
-      const acceptedTermsCell = newRow.insertCell();
-      acceptedTermsCell.textContent = userData.acceptedTerms ? 'Yes' : 'No';
-
-      // Add styling to cells
-      const cellStyle = 'border border-gray-300 p-2';
-      nameCell.className = cellStyle;
-      emailCell.className = cellStyle;
-      passwordCell.className = cellStyle;
-      dobCell.className = cellStyle;
-      acceptedTermsCell.className = cellStyle;
+      // Add the user data to the table
+      addRowToTable(userTableBody, userData);
     }
   }
 });
 
-const registrationForm = document.getElementById('registrationForm');
-registrationForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+// Event listener for form submission
+const form = document.getElementById('registrationForm');
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent default form submission
 
-  // Get user data from the form
-  const name = registrationForm.elements.name.value;
-  const email = registrationForm.elements.email.value;
-  const password = registrationForm.elements.password.value;
-  const dob = registrationForm.elements.dob.value;
-  const acceptedTerms = registrationForm.elements.acceptedTerms.checked;
+  // Get the user's date of birth from the form
+  const dob = new Date(form.dob.value);
 
-  // Validate age to accept users between 18 and 55 years old
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-  const birthDate = new Date(dob);
-  const age = currentYear - birthDate.getFullYear();
-  if (age < 18 || age > 55) {
+  // Calculate the user's age based on the current year
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - dob.getFullYear();
+
+  // Validate the user's age to ensure it's between 18 and 55
+  if (!(age >= 18 && age <= 55)) {
     alert('Age should be between 18 and 55.');
     return;
   }
 
-  // Generate a unique user key
+  // Generate a unique user key using the current timestamp
   const userKey = 'user_' + Date.now();
 
-  // Store user data in localStorage
-  localStorage.setItem(userKey, JSON.stringify({
-    name: name,
-    email: email,
-    password: password,
-    dob: dob,
-    acceptedTerms: acceptedTerms
-  }));
+  // Create a user object with the form data
+  const userData = {
+    name: form.name.value,
+    email: form.email.value,
+    password: form.password.value,
+    dob: form.dob.value,
+    acceptedTerms: form.acceptedTerms.checked,
+  };
 
-  // Create a new table row for the newly registered user
-  const newRow = userTableBody.insertRow();
+  // Store the user data in localStorage using the generated key
+  localStorage.setItem(userKey, JSON.stringify(userData));
 
-  // Add cells for user data
-  const nameCell = newRow.insertCell();
-  nameCell.textContent = name;
-  const emailCell = newRow.insertCell();
-  emailCell.textContent = email;
-  const passwordCell = newRow.insertCell();
-  passwordCell.textContent = password;
-  const dobCell = newRow.insertCell();
-  dobCell.textContent = dob;
-  const acceptedTermsCell = newRow.insertCell();
-  acceptedTermsCell.textContent = acceptedTerms ? 'Yes' : 'No';
-
-  // Add styling to cells
-  const cellStyle = 'border border-gray-300 p-2';
-  nameCell.className = cellStyle;
-  emailCell.className = cellStyle;
-  passwordCell.className = cellStyle;
-  dobCell.className = cellStyle;
-  acceptedTermsCell.className = cellStyle;
+  // Add the user data to the table
+  addRowToTable(userTableBody, userData);
 });
+
+// Function to add a new user row to the table
+function addRowToTable(tableBody, userData) {
+  // Create a new table row
+  const newRow = tableBody.insertRow();
+
+  // Set a consistent style for all cells
+  const cellStyle = 'border border-gray-300 p-2';
+
+  // Add cells for each user data property
+  const nameCell = newRow.insertCell();
+  nameCell.textContent = userData.name;
+  nameCell.className = cellStyle;
+
+  const emailCell = newRow.insertCell();
+  emailCell.textContent = userData.email;
+  emailCell.className = cellStyle;
+
+  const passwordCell = newRow.insertCell();
+  passwordCell.textContent = userData.password;
+  passwordCell.className = cellStyle;
+
+  const dobCell = newRow.insertCell();
+  dobCell.textContent = userData.dob;
+  dobCell.className = cellStyle;
+
+  const acceptedTermsCell = newRow.insertCell();
+  acceptedTermsCell.textContent = userData.acceptedTerms;
+  acceptedTermsCell.className = cellStyle;
+}
